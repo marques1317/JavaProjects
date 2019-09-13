7812150796865e53eaf2c8e5b2570d46c244834a
marques1317 <mmathis16@gmail.com>
2019-09-13 16:18:34 -0400

    Exercise 1.5

---
+++ Homework/Exercise 1.5/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("(9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5)");
+        System.out.println("Result:  " + (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+    }
+}
